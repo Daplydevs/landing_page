1946f815c2f270b66176d2c8de950f24ce599825
mr-dap <139409418+mr-dap@users.noreply.github.com>
2024-05-04 06:54:14 -0400

    sitemap10

---
+++ .history/pages/[[...catchall]]_20240504065406.tsx
@@ -0,0 +1,63 @@
+// pages/[[...catchall]].tsx
+
+import * as React from "react";
+import { useRouter } from "next/router";
+import {
+  PlasmicComponent,
+  extractPlasmicQueryData,
+  ComponentRenderData,
+  PlasmicRootProvider,
+} from "@plasmicapp/loader-nextjs";
+import Error from "next/error";
+import { PLASMIC } from "@/plasmic-init";
+
+export default function CatchAllPage(props: {
+  plasmicData?: ComponentRenderData;
+  queryCache?: Record<string, any>;
+}) {
+  const { plasmicData, queryCache } = props;
+  const router = useRouter();
+  if (!plasmicData || plasmicData.entryCompMetas.length === 0) {
+    return <Error statusCode={404} />;
+  }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  return (
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      prefetchedQueryData={queryCache}
+      pageRoute={pageMeta.path}
+      pageParams={pageMeta.params}
+      pageQuery={router.query}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { catchall } = context.params ?? {};
+  let plasmicPath = '';
+  if (catchall) {
+    plasmicPath = typeof catchall === 'string' ? catchall : `/${catchall.join('/')}`;
+  }
+  const plasmicData = await PLASMIC.maybeFetchComponentData("/content" + plasmicPath);
+  if (!plasmicData) {
+    // non-Plasmic catch-all
+    return { props: {} };
+  }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  // Cache the necessary data fetched for the page
+  const queryCache = await extractPlasmicQueryData(
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      pageRoute={pageMeta.path}
+      pageParams={pageMeta.params}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+  // Use revalidate if you want incremental static regeneration
+  return { props: { plasmicData, queryCache }, revalidate: 60 };
+};
