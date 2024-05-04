7bc8136fbc032231cad5e5f5cfe5d7bfa3a6a5dd
mr-dap <139409418+mr-dap@users.noreply.github.com>
2024-05-03 20:42:11 -0400

    sitemapx0

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -33,21 +33,41 @@ export default function PlasmicLoaderPage(props: {
     </PlasmicRootProvider>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { catchall } = context.params ?? {};
+  const plasmicPath = typeof catchall === 'string' ? catchall : Array.isArray(catchall) ? `/${catchall.join('/')}` : '/';
+  const plasmicData = await PLASMIC.maybeFetchComponentData("/content" + plasmicPath);
+  if (!plasmicData) {
+    // non-Plasmic catch-all
+    return { props: {} };
+  }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  // Cache the necessary data fetched for the page
+  const queryCache = await extractPlasmicQueryData(
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      pageRoute={pageMeta.path}
+      pageParams={pageMeta.params}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+  // Use revalidate if you want incremental static regeneration
+  return { props: { plasmicData, queryCache }, revalidate: 60 };
+}
+
 export const getStaticPaths: GetStaticPaths = async () => {
   try {
     const pageModules = await PLASMIC.fetchPages();
-    const uniquePaths = new Set();
-
-    // Map the paths and filter out duplicates
-    const paths = pageModules.reduce<{ params: { catchall: string[] } }[]>((acc, mod) => {
-      const catchall = mod.path.substring(1).split("/");
-      const pathString = catchall.join("/");
-      if (!uniquePaths.has(pathString)) {
-        uniquePaths.add(pathString);
-        acc.push({ params: { catchall } });
-      }
-      return acc;
-    }, []);
+    const paths = pageModules.map((mod, index) => {
+      // Define catch-all only for specific paths, excluding the root
+      const catchall = mod.path === "/" ? [] : mod.path.substring(1).split("/");
+      // Append a unique identifier to the last segment of the catchall parameter
+      catchall[catchall.length - 1] = `${catchall[catchall.length - 1]}_${index}`;
+      return { params: { catchall } };
+    });
 
     return {
       paths,
